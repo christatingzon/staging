6a5f7be39d578a3f79e1c8819861b5569de6c747
Christa <christatingzon@gmail.com>
2021-01-23 00:23:55 +0800

    Update to Project 3

--- realfastsocialgraphics/assets/js/main.js
+++ realfastsocialgraphics/assets/js/main.js
@@ -1,1 +1,18 @@
-//console.log('test');
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $(".scroll-to").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+    
+  });
+});
